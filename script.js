c6e6d736077d5082930db7610a32b001a55807e5
Sahil Randhawa <sahildeep.randhawa@gmail.com>
2024-02-29 02:23:42 -0500

    setinterval added

--- script.js
+++ script.js
@@ -6,12 +6,15 @@ const cloud = clouds[0];
 // cloud.style.left = `${(Math.random() * 100)}%`;
 // cloud.style.transform = `scale(${(Math.random() * 0.7) + 0.3})`;
 
-cloud.style.opacity = (Math.random() * 0.5) + 0.5;
 cloud.style.top = `200px`;
 cloud.style.left = `200px`;
 cloud.style.transform = `scale(0.5)`;
-cloud.style.transition = `all linear 15s`;
+cloud.style.transition = `all linear 3s`;
 
-window.onload = () => {
-    cloud.style.transform = `translateX(100vw)`;
-}
+setInterval(() => {
+    if (cloud.style.left === `100%`) {
+        cloud.style.left = `-100px`;
+    } else {
+        cloud.style.left = `100%`;
+    }
+}, 3000);
